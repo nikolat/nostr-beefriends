5147784a7306b8fea06222ce19ddb8504ef1fd3a
Don <496156+nikolat@users.noreply.github.com>
2024-09-07 19:48:36 +0900

    separate modes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { base } from './base.ts';
 import { Mode } from './utils.ts';
 
 Deno.serve(async (req) => {
-  console.log('Method:', req.method);
   if (req.method !== 'POST') {
     const body = JSON.stringify({ error: 'Method Not Allowed' });
     return new Response(body, {
@@ -15,7 +14,30 @@ Deno.serve(async (req) => {
   }
   if (req.body) {
     const body = await req.text();
-    const mode: Mode = Mode.Normal;
+    const url = new URL(req.url);
+    let mode: Mode | undefined;
+    switch (url.pathname) {
+      case '/normal':
+        mode = Mode.Normal;
+        break;
+      case '/reply':
+        mode = Mode.Reply;
+        break;
+      case '/fav':
+        mode = Mode.Fav;
+        break;
+      default:
+        break;
+    }
+    if (mode === undefined) {
+      const body = JSON.stringify({ error: '404 not found' });
+      return new Response(body, {
+        status: 404,
+        headers: {
+          'content-type': 'application/json; charset=utf-8',
+        },
+      });
+    }
     return await base(body, mode);
   } else {
     const body = JSON.stringify({ error: 'message body is not found' });
